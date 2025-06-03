4fa0919407cf09337112a12fee27120a9fec1a23
winfredo <winfrednukpe2002@gmail.com>
2025-06-03 12:58:42 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let fieldOne = document.querySelector(".field-one");
+let fieldTwo = document.querySelector(".field-two");
+
+function generatePassword() {
+  let passwordOne = "";
+  let passwordTwo = "";
+  let passwordlength = 15;
+  for (let i = 0; i < passwordlength; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    let randomIndex2 = Math.floor(Math.random() * characters.length);
+    passwordOne += characters[randomIndex];
+    passwordTwo += characters[randomIndex2];
+    fieldOne.textContent = passwordOne;
+    fieldTwo.textContent = passwordTwo;
+  }
+}
+
+function reset() {
+  fieldOne.textContent = "";
+  fieldTwo.textContent = "";
+}
